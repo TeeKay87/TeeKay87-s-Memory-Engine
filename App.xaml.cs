4dd96efbec8017774d43be373c85d5705f58fd2f
TeeKay87 <pcgmr87@gmail.com>
2026-08-30 00:36:06 +0200

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TeeKay87_s_Memory_Engine
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
